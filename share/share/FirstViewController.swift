d51456c3f0c8061e988d19a96348583a841cf0b0
hyhSuper <975348978@qq.com>
2015-11-09 23:53:25 +0800

    新建

---
+++ share/share/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  share
+//
+//  Created by Allan on 15/11/9.
+//  Copyright © 2015年 Allan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var firstNavBar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        self.title = "Share"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ share/share/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  share
+//
+//  Created by Allan on 15/11/9.
+//  Copyright © 2015年 Allan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
